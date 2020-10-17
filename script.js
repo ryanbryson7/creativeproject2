47dc88dfe778f852564e748866d631c2c54a266e
Ryan <ryanbryson7@gmail.com>
2020-10-17 13:28:44 -0600

    Loading the rest of the templates for each file

---
+++ script.js
@@ -0,0 +1,44 @@
+document.getElementById("weatherSubmit").addEventListener("click", function(event) {
+  event.preventDefault();
+	const value = document.getElementById("weatherInput").value;
+  if (value === "")
+    return;
+  console.log(value);
+
+	const url = "http://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=fd3bf803206a41f0fc464f40beee8659";
+	fetch(url)
+		.then(function(response) {
+			return response.json();
+		}).then(function(json) {
+			let results = "";
+			results += '<h2>Weather in ' + json.name + "</h2>";
+			for (let i=0; i < json.weather.length; i++) {
+				results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
+			}
+			results += '<h2>' + json.main.temp + " &deg;F</h2>";
+			results += "<p>Feels Like: " + json.main.feels_like + "</p>";
+			results += "<p>"
+			for (let i=0; i < json.weather.length; i++) {
+				results += json.weather[i].description
+				if (i !== json.weather.length - 1)
+					results += ", "
+			}
+			results += "</p>";
+			results += "<p>Wind Speed: " + json.wind.speed +  "</p>";
+			document.getElementById("weatherResults").innerHTML = results;
+		});
+
+		const url2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial" + "&APPID=fd3bf803206a41f0fc464f40beee8659";
+		fetch(url2)
+		  .then(function(response) {
+		    return response.json();
+		  }).then(function(json) {
+				let forecast = "";
+				for (let i=0; i < json.list.length; i++) {
+					forecast += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</h2>";
+					forecast += "<p>Temperature: " + json.list[i].main.temp + " °F" + "</p>";
+					forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>';
+				}
+				document.getElementById("forecastResults").innerHTML = forecast;
+		  });
+});
