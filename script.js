0ccd180f3735adc9c4925832df3e4e2fd1f95c4b
Ryan <ryanbryson7@gmail.com>
2020-10-17 18:29:06 -0600

    Final commit

--- script.js
+++ script.js
@@ -1,44 +1,23 @@
-document.getElementById("weatherSubmit").addEventListener("click", function(event) {
+document.getElementById("quoteSubmit").addEventListener("click", function(event) {
   event.preventDefault();
-	const value = document.getElementById("weatherInput").value;
-  if (value === "")
-    return;
-  console.log(value);
+	const value = document.getElementById("quoteNumberInput").value;
 
-	const url = "http://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=fd3bf803206a41f0fc464f40beee8659";
+
+	const url = "https://ron-swanson-quotes.herokuapp.com/v2/quotes/" + value;
 	fetch(url)
 		.then(function(response) {
 			return response.json();
 		}).then(function(json) {
 			let results = "";
-			results += '<h2>Weather in ' + json.name + "</h2>";
-			for (let i=0; i < json.weather.length; i++) {
-				results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
+			results += "<h2>Quotes</h2>";
+			if (value === "") {
+				results += "<p><strong>Quote #1: </strong>\"" + json[0] + "\"</p>";
 			}
-			results += '<h2>' + json.main.temp + " &deg;F</h2>";
-			results += "<p>Feels Like: " + json.main.feels_like + "</p>";
-			results += "<p>"
-			for (let i=0; i < json.weather.length; i++) {
-				results += json.weather[i].description
-				if (i !== json.weather.length - 1)
-					results += ", "
-			}
-			results += "</p>";
-			results += "<p>Wind Speed: " + json.wind.speed +  "</p>";
-			document.getElementById("weatherResults").innerHTML = results;
-		});
-
-		const url2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial" + "&APPID=fd3bf803206a41f0fc464f40beee8659";
-		fetch(url2)
-		  .then(function(response) {
-		    return response.json();
-		  }).then(function(json) {
-				let forecast = "";
-				for (let i=0; i < json.list.length; i++) {
-					forecast += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</h2>";
-					forecast += "<p>Temperature: " + json.list[i].main.temp + " °F" + "</p>";
-					forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>';
+			else {
+				for (let i=0; i < value; i++) {
+					results += "<p><strong>Quote #" + (i+1) + ":</strong> \"" + json[i] + "\"</p>";
 				}
-				document.getElementById("forecastResults").innerHTML = forecast;
-		  });
+			}
+			document.getElementById("quoteResults").innerHTML = results;
+		});
 });
